af609b4fe86730e5756a838ba6b297120a11e206
Tobias Pape <tobias@netshed.de>
2025-08-31 13:03:19 +0200

    remove unofficial IPv6 multicast.
    Makes everything simpler, too

--- src/main/java/net/redstonecraft/lanservers/LanServers.java
+++ src/main/java/net/redstonecraft/lanservers/LanServers.java
@@ -13,20 +13,15 @@ import java.util.stream.Collectors;
 public class LanServers {
 
     private final Timer timer = new Timer();
-    private final Inet4Address multicastAddress4 = (Inet4Address) InetAddress.getByName("224.0.2.60");
-    private final Inet6Address multicastAddress6 = (Inet6Address) InetAddress.getByName("ff75:230::60");
+    private final Inet4Address multicastAddress = (Inet4Address) InetAddress.getByName("224.0.2.60");
     private final static int multicastPort = 4445;
-    private final List<MulticastSocket> sources4 = LanServers.getSourceSockets(false);
-    private final List<MulticastSocket> sources6 = LanServers.getSourceSockets(true);
+    private final List<MulticastSocket> sources = LanServers.getSourceSockets();
     private final ILanServersPlugin plugin;
 
     public LanServers(ILanServersPlugin plugin) throws IOException {
         this.plugin = plugin;
-        for (MulticastSocket socket : sources4) {
-            socket.joinGroup(multicastAddress4);
-        }
-        for (MulticastSocket socket : sources6) {
-            socket.joinGroup(multicastAddress6);
+        for (MulticastSocket socket : sources) {
+            socket.joinGroup(multicastAddress);
         }
     }
 
@@ -35,16 +30,8 @@ public class LanServers {
             @Override
             public void run() {
                 byte[] data = ("[MOTD]" + plugin.getMotd() + "[/MOTD][AD]" + plugin.getPort() + "[/AD]").getBytes(StandardCharsets.UTF_8);
-                DatagramPacket packet = new DatagramPacket(data, data.length, multicastAddress4, multicastPort);
-                for (MulticastSocket socket :  sources4) {
-                    try {
-                        socket.send(packet);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                packet = new DatagramPacket(data, data.length, multicastAddress6, multicastPort);
-                for (MulticastSocket socket :  sources6) {
+                DatagramPacket packet = new DatagramPacket(data, data.length, multicastAddress, multicastPort);
+                for (MulticastSocket socket :  sources) {
                     try {
                         socket.send(packet);
                     } catch (IOException e) {
@@ -57,16 +44,12 @@ public class LanServers {
 
     public void disable() {
         timer.cancel();
-        for (MulticastSocket socket :  sources4) {
+        for (MulticastSocket socket :  sources) {
             socket.close();
         }
-        for (MulticastSocket socket :  sources6) {
-            socket.close();
-        }
-
     }
 
-    private static List<MulticastSocket> getSourceSockets(boolean doIPv6) {
+    private static List<MulticastSocket> getSourceSockets() {
         try {
             // Take all network interfaces
             return Collections.list(NetworkInterface.getNetworkInterfaces()).stream()
@@ -81,10 +64,8 @@ public class LanServers {
                     // interface can have multiple addresses, make a loooong list
                     .flatMap(iface -> iface.getInterfaceAddresses().stream()
                             // get actual address
-                            // select ipv6 if desired
                             // make socket, ignore errors
                             .map(ifaddr -> ifaddr.getAddress())
-                            .filter(inaddr -> (inaddr instanceof Inet6Address) == doIPv6)
                             .map(inaddr -> new InetSocketAddress(inaddr, multicastPort))
                             .map(insaddr -> {
                                 try {
